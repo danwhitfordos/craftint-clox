887245ed6a31ce1fa4a7be30e6e0745ad15e310c
Dan Whitford <dan.whitford@os.uk>
2026-06-01 15:06:59 +0100

    convenienve run step

--- build.zig
+++ build.zig
@@ -85,4 +85,12 @@ pub fn build(b: *std.Build) void {
     exe.root_module.addIncludePath(.{ .cwd_relative = "." });
 
     b.installArtifact(exe);
+
+    const run_step = b.step("run", "Run the app");
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
 }
